d94ab4e8fe7fca3bfd19afed83ca27e8a753f705
hassancodess <dev.hassancodes@gmail.com>
2022-09-14 13:56:50 +0500

    Add Tailwind CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        dark: '#121A34',
+      },
+    },
+  },
+  plugins: [],
+}
